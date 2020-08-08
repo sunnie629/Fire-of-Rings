6462db3fa0def13308bb208b98c81db1c9c185bc
Sunnie Kwak <sunnie@live.unc.edu>
2020-08-08 17:02:41 -0400

    everything that just got deleted

--- frontend/src/Components/joinRoom.js
+++ frontend/src/Components/joinRoom.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import io from "socket.io-client";
+
+let socket;
+const endpoint = "localhost:5000";
 
 class JoinRoom extends Component {
-  state = {};
+  state = {
+    name: "",
+    roomCode: "",
+    users: [],
+    sumbitted: false,
+  };
+
+  componentDidMount() {
+    socket = io(endpoint);
+    socket.on("usersUpdated", ({ usersarr }) => {
+      this.setState({ users: usersarr });
+    });
+  }
+
+  componentWillUnmount() {
+    socket.emit("disconnect");
+    socket.off();
+  }
+
+  handleSubmit = () => {
+    socket.emit("join", {
+      name: this.state.name,
+      roomCode: this.state.roomCode,
+    });
+    this.setState({ submitted: true });
+  };
+
+  handleChangeName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleChangeRoom = (event) => {
+    this.setState({ roomCode: event.target.value });
+  };
+
   render() {
     return (
       <div>
@@ -10,6 +48,43 @@ class JoinRoom extends Component {
           <button>Back</button>
         </Link>
         <h1>JoinRoom</h1>
+        {this.state.submitted ? (
+          <div>
+            <h1>Room code: {this.state.roomCode}</h1>
+            <ul>
+              {this.state.users.map((user) => {
+                return <li>{user.name}</li>;
+              })}
+            </ul>
+            <Link to="/game">
+              <button>Start Game</button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <form>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleChangeName}
+                />
+              </label>
+              <label>
+                Room Code:
+                <input
+                  type="text"
+                  name="roomCode"
+                  value={this.state.roomCode}
+                  onChange={this.handleChangeRoom}
+                />
+              </label>
+            </form>
+            <button onClick={this.handleSubmit}>Submit</button>
+          </div>
+        )}
       </div>
     );
   }
